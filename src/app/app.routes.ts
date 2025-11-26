9bd1a3468935fe66d983419a43067246e346fba5
Hugo Veiga <141176099+HugoVS26@users.noreply.github.com>
2025-11-26 15:00:36 +0100

    add debt detail page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import type { Routes } from '@angular/router';
 import { DebtListPage } from './pages/debt-list/debt-list';
 import { DebtFormPage } from './pages/debt-form/debt-form';
+import { DebtDetailPage } from './pages/debt-detail/debt-detail';
 
 export const routes: Routes = [
   { path: '', component: DebtListPage },
   { path: 'debts', component: DebtListPage },
   { path: 'debts/new', component: DebtFormPage },
   { path: 'debts/:debtId/edit', component: DebtFormPage },
+  { path: 'debts/:debtId', component: DebtDetailPage },
 ];
