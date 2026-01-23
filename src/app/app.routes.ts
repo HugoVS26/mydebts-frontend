812d97ce4b75d336133f74afcb396c953540f270
Hugo Veiga <141176099+HugoVS26@users.noreply.github.com>
2026-01-23 16:46:00 +0100

    create register page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import type { Routes } from '@angular/router';
 import { DebtListPage } from './pages/debt-list/debt-list';
 import { DebtFormPage } from './pages/debt-form/debt-form';
 import { DebtDetailPage } from './pages/debt-detail/debt-detail';
+import { RegisterPage } from './pages/register/register';
 
 export const routes: Routes = [
   { path: '', component: DebtListPage },
@@ -9,4 +10,5 @@ export const routes: Routes = [
   { path: 'debts/new', component: DebtFormPage },
   { path: 'debts/:debtId/edit', component: DebtFormPage },
   { path: 'debts/:debtId', component: DebtDetailPage },
+  { path: 'register', component: RegisterPage },
 ];
